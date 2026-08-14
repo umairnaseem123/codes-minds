517349d02c7bff971f33c4c56c8f32ed8c2805b0
Umair Nasim <umairnasim26@gmail.com>
2026-08-14 18:13:01 +0500

    Fix mobile overlay positioning outside header

--- codes-minds/src/components/Header.jsx
+++ codes-minds/src/components/Header.jsx
@@ -31,76 +31,84 @@ function Header() {
   }, [mobileOpen]);
 
   return (
-    <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
-      <div className="container header__inner">
-        <Link to="/" className="header__logo">
-          <img src={logo} alt="CØDES-MINDS" />
-        </Link>
-
-        <div
-          className={`header__overlay ${
-            mobileOpen ? "header__overlay--visible" : ""
-          }`}
-          onClick={() => setMobileOpen(false)}
-        />
-
-        <nav className={`header__nav ${mobileOpen ? "header__nav--open" : ""}`}>
-          <NavLink to="/" className="header__link" end>
-            Home
-          </NavLink>
-          <NavLink to="/about" className="header__link">
-            About Us
-          </NavLink>
-
-          <div
-            className="header__dropdown"
-            onMouseEnter={() => setServicesOpen(true)}
-            onMouseLeave={() => setServicesOpen(false)}
-          >
-            <NavLink
-              to="/services"
-              className="header__link header__link--dropdown"
-            >
-              Services{" "}
-              <ChevronDown size={16} className={servicesOpen ? "rotate" : ""} />
-            </NavLink>
-
-            {servicesOpen && (
-              <div className="header__dropdown-menu">
-                {services.map((service) => (
-                  <Link
-                    key={service.slug}
-                    to={`/services/${service.slug}`}
-                    className="header__dropdown-item"
-                  >
-                    <service.icon size={16} />
-                    {service.title}
-                  </Link>
-                ))}
-              </div>
-            )}
-          </div>
-
-          <NavLink to="/contact" className="header__link">
-            Contact
-          </NavLink>
-        </nav>
-
-        <div className="header__actions">
-          <Link to="/contact" className="btn btn--primary header__cta">
-            Get In Touch <ChevronDown size={16} className="header__cta-arrow" />
+    <>
+      <header className={`header ${scrolled ? "header--scrolled" : ""}`}>
+        <div className="container header__inner">
+          <Link to="/" className="header__logo">
+            <img src={logo} alt="CØDES-MINDS" />
           </Link>
 
-          <button
-            className="header__toggle"
-            onClick={() => setMobileOpen(!mobileOpen)}
-            aria-label="Toggle menu"
+          <nav
+            className={`header__nav ${mobileOpen ? "header__nav--open" : ""}`}
           >
-            {mobileOpen ? <X size={26} /> : <Menu size={26} />}
-          </button>
+            <NavLink to="/" className="header__link" end>
+              Home
+            </NavLink>
+            <NavLink to="/about" className="header__link">
+              About Us
+            </NavLink>
+
+            <div
+              className="header__dropdown"
+              onMouseEnter={() => setServicesOpen(true)}
+              onMouseLeave={() => setServicesOpen(false)}
+            >
+              <NavLink
+                to="/services"
+                className="header__link header__link--dropdown"
+              >
+                Services{" "}
+                <ChevronDown
+                  size={16}
+                  className={servicesOpen ? "rotate" : ""}
+                />
+              </NavLink>
+
+              {servicesOpen && (
+                <div className="header__dropdown-menu">
+                  {services.map((service) => (
+                    <Link
+                      key={service.slug}
+                      to={`/services/${service.slug}`}
+                      className="header__dropdown-item"
+                    >
+                      <service.icon size={16} />
+                      {service.title}
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+
+            <NavLink to="/contact" className="header__link">
+              Contact
+            </NavLink>
+          </nav>
+
+          <div className="header__actions">
+            <Link to="/contact" className="btn btn--primary header__cta">
+              Get In Touch{" "}
+              <ChevronDown size={16} className="header__cta-arrow" />
+            </Link>
+
+            <button
+              className="header__toggle"
+              onClick={() => setMobileOpen(!mobileOpen)}
+              aria-label="Toggle menu"
+            >
+              {mobileOpen ? <X size={26} /> : <Menu size={26} />}
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <div
+        className={`header__overlay ${
+          mobileOpen ? "header__overlay--visible" : ""
+        }`}
+        onClick={() => setMobileOpen(false)}
+      />
+    </>
   );
 }
 
